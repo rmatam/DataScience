501ac645b79d6795f0373a10f5ea0f89d8e94062
rmatam <sekhar.matam@gmail.com>
2016-11-01 13:37:41 +0530

    Create findCorrelation.R

---
+++ 01.Algorithms/09.Feature-Engineering/findCorrelation.R
@@ -0,0 +1,20 @@
+install.packages("mlbench")
+install.packages("caret")
+# ensure the results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the data
+data(PimaIndiansDiabetes)
+PimaIndiansDiabetes
+dim(PimaIndiansDiabetes)
+str(PimaIndiansDiabetes)
+# calculate correlation matrix
+correlationMatrix <- cor(PimaIndiansDiabetes[,1:8])
+# summarize the correlation matrix
+print(correlationMatrix)
+# find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
